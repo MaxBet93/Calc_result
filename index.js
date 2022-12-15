db98135272f87a5353fb2278908c7ce572cd9955
MaxBet93 <114855423+MaxBet93@users.noreply.github.com>
2022-12-15 17:55:12 +0200

    Calc

---
+++ index.js
@@ -0,0 +1,20 @@
+const calc = document.querySelector('.calc');
+
+calc.addEventListener('click',function(event){
+  if (!event.target.classList.contains('calc_btn')) return;
+   
+  const value = event.target.innerText;
+  switch(value){
+    case 'C':
+      result.innerText = '';
+      break;
+    
+    case '=':
+      result.innerText  = eval(result.innerText);
+      break;
+
+    default:
+      result.innerText += value;
+  }
+
+});
